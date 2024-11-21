16e3ec8e98d4e1fcca0bbfe6d3ef44e7ba7f01da
mansoor <mansoorshaik7b@gmail.com>
2024-11-21 11:21:12 +0530

    resume updaated

--- frontend/src/components/ResumeButton.jsx
+++ frontend/src/components/ResumeButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ResumeButton = () => {
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/Resume.pdf";
+    link.href = "/MansoorResume.pdf";
     link.download = "Mansoor's Resume.pdf";
     document.body.appendChild(link);
     link.click();
